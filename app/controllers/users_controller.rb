6abaa07ee09339f8cbb830ee7a5a50a42162caf5
unohayato <mamidori1p1p@gmail.com>
2024-04-03 06:35:48 +0900

    :fire:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end

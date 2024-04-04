182165a2abb3ad43082fcb4547d82e6ca4d790d2
unohayato <mamidori1p1p@gmail.com>
2024-04-04 11:02:58 +0900

    :fire:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       reset_session      # ログインの直前に必ずこれを書くこと
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else

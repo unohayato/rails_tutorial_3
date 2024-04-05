454dbb934b4a3a473e702f3d97185739e3f3effa
unohayato <mamidori1p1p@gmail.com>
2024-04-05 17:46:21 +0900

    :fire:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      if user.activated?
+      if @user.activated?
         forwarding_url = session[:forwarding_url]
         reset_session
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        log_in user
-        redirect_to forwarding_url || user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        log_in @user
+        redirect_to forwarding_url || @user
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -73,27 +73,6 @@ class LogoutTest < Logout
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
-  test "login with valid information followed by logout" do
-    post login_path, params: { session: { email:    @user.email, password: 'password' } }
-    assert is_logged_in?
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
-    delete logout_path
-    assert_response :see_other
-    assert_not is_logged_in?
-    assert_redirected_to root_url
-    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
-    delete logout_path
-    follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
-  end
-
   test "should still work after logout in second window" do
     delete logout_path
     assert_redirected_to root_url
@@ -104,8 +83,7 @@ class RememberingTest < UsersLogin
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    # assert_not cookies[:remember_token].blank?
-    assert_equal false, cookies[:remember_token].blank?
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do

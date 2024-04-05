981c13df620710199821f9569e2fc97151098763
unohayato <mamidori1p1p@gmail.com>
2024-04-05 10:48:12 +0900

    :fire:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -32,6 +34,16 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  # beforeフィルタ
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url, status: :see_other
     end
   end
 

f37486a91ec7b787b881612c6325a3e725d7eab1
unohayato <mamidori1p1p@gmail.com>
2024-04-12 19:03:02 +0900

    :fire:

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,7 +11,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", helf_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
@@ -24,7 +24,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links for logged-in user" do
     log_in_as(@user)
     get root_path
-    assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", users_path
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", edit_user_path(@user)
